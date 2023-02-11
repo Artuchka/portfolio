bce6ef994f0d235c2c6c619ad04e1fba1217b724
Temka Artemka <yandex949@gmail.com>
2023-02-11 23:47:02 +0300

    feaure: sliding projects with gestures

--- src/components/DetailsSlider.tsx
+++ src/components/DetailsSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { MouseEvent, useEffect, useRef, useState } from "react"
 import {
 	PanInfo,
 	motion,
@@ -25,6 +25,7 @@ const style = {
 		justifyContent: "center",
 		alignItems: "center",
 		overflow: "hidden",
+		// overflowX: "scroll",
 	}),
 	backdrop: css({
 		position: "fixed",
@@ -70,6 +71,10 @@ export const DetailsSlider = () => {
 	const [selectedProjectIndex, setSelectedProjectIndex] = useRecoilState(
 		selectedProjectIndexState
 	)
+	const [{ startX, isDragging }, setDragStart] = useState({
+		startX: 0,
+		isDragging: false,
+	})
 	const [pointerEvents, setPointerEvents] = useState(true)
 	console.log({ selectedProjectIndex })
 	const projects = useRecoilValue(projectsState)
@@ -77,24 +82,58 @@ export const DetailsSlider = () => {
 	const listingControls = useAnimation()
 	const offset = useMotionValue(0)
 
-	// const handlePanEnd = (e: any, info: PanInfo) => {
-	// 	// setPointerEvents(true)
-	// }
+	const showSelectedItem = () => {
+		const itemWidth = getItemWidth()
+		const newOffset =
+			(projects.length - selectedProjectIndex - initialOffset.current) *
+			itemWidth
 
-	// const handlePanStart = (e: any, info: PanInfo) => {
-	// 	// setPointerEvents(false)
-	// }
+		listingControls.start({ x: newOffset })
+	}
 
-	// const handlePan = (e: any, info: PanInfo) => {
-	// 	listingControls.set({ x: info.offset.x })
-	// }
+	const getItemWidth = () => {
+		const listingWidth = listingRef.current.getBoundingClientRect().width
+		const itemWidth = listingWidth / projects.length
+		return itemWidth
+	}
+
+	const handlePanEnd = (e: any, info: PanInfo) => {
+		const itemWidth = getItemWidth()
+
+		const createdOffset = info.offset.x
+
+		console.log("--------------------")
+		console.log({ itemWidth: itemWidth / 2 })
+		console.log({ createdOffset })
+		console.log("--------------------")
+		if (Math.abs(createdOffset) < 100) {
+			showSelectedItem()
+		}
+
+		if (createdOffset < itemWidth / 2) {
+			handleNext()
+		} else if (createdOffset > itemWidth / 2) {
+			handlePrev()
+		} else {
+			showSelectedItem()
+		}
+	}
+
+	const handlePanStart = (e: any, info: PanInfo) => {
+		// setPointerEvents(false)
+	}
+
+	const handlePan = (e: any) => {
+		// if (!isDragging) return
+		// listingControls.set({ x: info.offset.x })
+	}
 
 	useEffect(() => {
 		const foundIndex = projects.findIndex(
 			(item) => item.uuid === selectedProjectUUID
 		)
-		setSelectedProjectIndex(foundIndex)
-	}, [])
+		if (foundIndex !== -1) setSelectedProjectIndex(foundIndex)
+	}, [projects])
 
 	const handleNext = async () => {
 		setSelectedProjectIndex((prev) => {
@@ -130,7 +169,6 @@ export const DetailsSlider = () => {
 			initialOffset.current = projects.length / 2 + 0.5
 		}
 	}
-
 	useEffect(() => {
 		console.log({ len: projects.length })
 		calculateOffset()
@@ -139,23 +177,68 @@ export const DetailsSlider = () => {
 		if (initialOffset.current === 0) {
 			calculateOffset()
 		}
-		const listingWidth = listingRef.current.getBoundingClientRect().width
-		const itemWidth = listingWidth / projects.length
-		const newOffset =
-			(projects.length - selectedProjectIndex - initialOffset.current) *
-			itemWidth
 
-		console.log({ initialOffset: initialOffset.current })
-		listingControls.start({ x: newOffset })
+		showSelectedItem()
 	}, [selectedProjectIndex])
 
+	const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+		setDragStart({
+			startX: e.pageX,
+			isDragging: true,
+		})
+	}
+
+	const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+		if (!isDragging) return
+		const diff = (startX - e.pageX) / 50
+		// console.log({ setting: offset.get() - diff })
+
+		listingControls.set({ x: offset.get() - diff })
+	}
+
+	const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
+		if (!isDragging) return
+		setDragStart((prev) => ({ ...prev, isDragging: false }))
+		const diff = startX - e.pageX
+
+		const itemWidth = getItemWidth()
+
+		// console.log("--------------------")
+		// console.log({ itemWidth: itemWidth / 2 })
+		// console.log({ diff })
+		// console.log("--------------------")
+		if (Math.abs(diff) < 100) {
+			showSelectedItem()
+		}
+
+		if (diff < 0 && Math.abs(diff) > itemWidth / 4) {
+			handlePrev()
+		} else if (diff > 0 && diff > itemWidth / 4) {
+			handleNext()
+		} else {
+			showSelectedItem()
+		}
+	}
+
 	return (
-		<div className="layout-cards" css={style.wrapper}>
+		<div
+			className="layout-cards"
+			css={style.wrapper}
+			onMouseDown={handleMouseDown}
+			onMouseUp={handleMouseUp}
+			onMouseMove={handleMouseMove}
+		>
 			<motion.div
 				initial={{ opacity: 0 }}
 				animate={listingControls}
 				exit={{
 					opacity: 0,
+				}}
+				transition={{
+					type: "spring",
+					duration: 1,
+					stiffness: 300,
+					damping: 50,
 				}}
 				css={style.listing}
 				style={{
@@ -213,3 +296,68 @@ export const DetailsSlider = () => {
 		</div>
 	)
 }
+
+// const Flashcards = () => {
+// 	const [{ startX, startScrollLeft, isDragging }, setDragStart] = useState({
+// 		startX: 0,
+// 		startScrollLeft: 0,
+// 		isDragging: false,
+// 	})
+// 	const containerRef = useRef(document.createElement("div"))
+// 	const cardRefs = useRef(new Array())
+// 	useEffect(() => {
+// 		const { scrollWidth, clientWidth } = containerRef.current
+// 		const halfScroll = (scrollWidth - clientWidth) / 2
+// 		containerRef.current.scrollLeft = halfScroll
+// 	}, [containerRef.current])
+
+// 	const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+// 		setDragStart({
+// 			startX: e.pageX - containerRef.current.offsetLeft,
+// 			startScrollLeft: containerRef?.current?.scrollLeft,
+// 			isDragging: true,
+// 		})
+// 	}
+// 	const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+// 		if (!isDragging || selectedCard) return
+// 		const x = e.pageX - containerRef.current.offsetLeft
+// 		const walk = x - startX
+// 		containerRef.current.scrollLeft = startScrollLeft - walk
+// 	}
+// 	const selectCard = (card) => {
+// 		cardRefs.current[card - 1].scrollIntoView({
+// 			behavior: "smooth",
+// 			block: "nearest",
+// 			inline: "center",
+// 		})
+// 	}
+// 	const handleCardMouseUp = (e: MouseEvent<HTMLDivElement>, card) => {
+// 		if (isDragging) {
+// 			const x = e.pageX - containerRef.current.offsetLeft
+// 			const walk = x - startX
+// 			if (Math.abs(walk) < 5) selectCard(card)
+// 		} else selectCard(card)
+// 	}
+// 	return (
+// 		<div
+// 			className="flashcards"
+// 			onMouseDown={handleMouseDown}
+// 			onMouseUp={() =>
+// 				setDragStart((prev) => ({ ...prev, isDragging: false }))
+// 			}
+// 			onMouseMove={handleMouseMove}
+// 		>
+// 			<div className="flashcards__container" ref={containerRef}>
+// 				{cards.map((card, i) => (
+// 					<motion.div
+// 						className="card"
+// 						key={card}
+// 						ref={(el) => cardRefs.current.push(el)}
+// 						onMouseUp={(e) => handleCardMouseUp(e, card)}
+
+// 					/>
+// 				))}
+// 			</div>
+// 		</div>
+// 	)
+// }
